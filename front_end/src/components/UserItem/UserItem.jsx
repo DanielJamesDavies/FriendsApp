c4a8fbd663a2cfb700d5416dacef48a8316b81b7
Daniel Davies <danieljamesdavies12@gmail.com>
2021-07-20 17:12:25 +0100

    User Item UI Update

--- front_end/src/components/UserItem/UserItem.jsx
+++ front_end/src/components/UserItem/UserItem.jsx
@@ -16,57 +16,46 @@ export const UserItem = ({ user, isFriend }) => {
 	const { toUser, compatibilityValueClassName } = UserItemLogic();
 
 	return (
-		<div className='user-item-container'>
-			<div
-				className={
-					user.backgroundImage !== undefined
-						? "user-item user-item-with-background-image"
-						: "user-item user-item-without-background-image"
-				}
-				onClick={() => toUser(user.username)}
-			>
-				{/* {user.backgroundImage === undefined ? null :
-                    <div className="user-item-background-container">
-                        <div className="user-item-with-background-image-hover-filter" />
-                        <img src={user.backgroundImage}
-                            className="user-item-background"
-                            alt=""
-                        />
-                    </div>
-                } */}
-
-				<div className='user-item-foreground'>
-					<div className='user-item-profile-picture'>
-						<svg height='50' width='50'>
-							<circle cx='25' cy='25' r='25' />
-						</svg>
-					</div>
-
-					<div className='user-item-names'>
-						<p className='user-item-nickname'>{user.nickname}</p>
-						<p className='user-item-username'>@{user.username}</p>
-
-						{!isFriend ? null : (
-							<p
-								className={
-									user.status !== "Offline"
-										? "user-item-status user-item-status-online"
-										: "user-item-status user-item-status-offline"
-								}
-							>
-								{user.status}
-							</p>
-						)}
-					</div>
-
-					{isFriend || user.compatibility === undefined ? null : (
-						<div className='user-item-compatibility'>
-							<p className='user-item-compatibility-label'>Compatibility</p>
-							<p className={compatibilityValueClassName(user.compatibility)}>{user.compatibility}%</p>
-						</div>
-					)}
-				</div>
+		<div className='user-item' onClick={() => toUser(user.username)}>
+			<div className='user-item-profile-picture'>
+				<svg height='50' width='50'>
+					<circle cx='25' cy='25' r='25' />
+				</svg>
 			</div>
+
+			<div className='user-item-names'>
+				<p className='user-item-nickname'>{user.nickname}</p>
+				<p className='user-item-username'>@{user.username}</p>
+			</div>
+
+			<div className='user-item-second-info'>
+				<p className='user-item-bio'>{user.bio}</p>
+
+				{!isFriend ? null : (
+					<p
+						className={
+							user.status !== "Offline" ? "user-item-status user-item-status-online" : "user-item-status user-item-status-offline"
+						}
+					>
+						{user.status}
+					</p>
+				)}
+
+				{isFriend || user.compatibility === undefined ? null : (
+					<div className='user-item-compatibility'>
+						<p className='user-item-compatibility-label'>
+							Compatibility: <span className={compatibilityValueClassName(user.compatibility)}>{user.compatibility}%</span>
+						</p>
+					</div>
+				)}
+			</div>
+
+			{user.banner === undefined ? null : (
+				<div className='user-item-banner-container'>
+					<div className='user-item-banner-hover-filter' />
+					<img src={user.banner} className='user-item-banner' alt='' />
+				</div>
+			)}
 		</div>
 	);
 };
